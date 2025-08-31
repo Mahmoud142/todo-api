a2fa6f503b470db98aae9c9e04fb7b91a4a9657b
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-09-01 00:28:31 +0300

    Apply express-handler to login and signup

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,59 +1,46 @@
 const prisma = require("../config/db");
 const bcrypt = require("bcrypt");
+const asyncHandler = require("express-async-handler");
 
-exports.signUp = async (req, res, next) => {
-  try {
-    console.log(req.body);
-    const { username, email, name, password } = req.body;
-    const user = await prisma.user.create({
-      data: {
-        username,
-        email,
-        name,
-        password: await bcrypt.hash(password, 10),
-      },
-    });
-    delete user.password;
-    res.status(201).json({
-      status: "success",
-      msg: "User created successfully",
-      data: user,
-    });
-  } catch (error) {
-    console.error("Error in signUp:", error);
-    res.status(500).json({
-      status: "error",
-      error: "Internal Server Error from signUp",
-    });
+
+
+exports.signUp = asyncHandler(async (req, res, next) => {
+  const user = await prisma.user.create({
+    data: {
+      username: req.body.username,
+      email: req.body.email,
+      name: req.body.name,
+      password: await bcrypt.hash(req.body.password, 10),
+    },
+  });
+  delete user.password;
+  res.status(201).json({
+    status: "success",
+    msg: "User created successfully",
+    data: user,
+  });
+});
+
+
+
+exports.login = asyncHandler(async (req, res, next) => {
+  
+  const user = await prisma.user.findUnique({ where: { email: req.body.email } });
+  let isPasswordMatch = false;
+  //find user
+  if (user) {
+    isPasswordMatch = await bcrypt.compare(req.body.password, user.password);
   }
-};
-
-exports.login = async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
-    const user = await prisma.user.findUnique({ where: { email } });
-    let isPasswordMatch = false;
-    if (user) {
-      isPasswordMatch = await bcrypt.compare(password, user.password);
-    }
-    if (!isPasswordMatch || !user) {
-      return res.status(401).json({ error: "Invalid email or password" });
-    }
-
-    delete user.password;
-    
-    
-
-    res.status(200).json({
-      status: "success",
-      msg: "User logged in successfully",
-      data: user,
-    });
-  } catch (error) {
-    console.error("Error in login:", error);
-    res.status(500).json({
-      status: "error",
-      error: "Internal Server Error from login",
-    });
+  // check if password and user exist
+  if (!isPasswordMatch || !user) {
+    return res.status(401).json({ error: "Invalid email or password" });
   }
-};
+
+  delete user.password;
+
+  res.status(200).json({
+    status: "success",
+    msg: "User logged in successfully",
+    data: user,
+  });
+});

2eb6e42b6dbcea163a8634c23ed1b6cb21c3d55c
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-09-02 02:39:37 +0300

    update API Documentation

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,11 @@
 const prisma = require("../config/db");
 const bcrypt = require("bcrypt");
 const asyncHandler = require("express-async-handler");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-
+//@desc Sign up a new user
+//@route POST /api/auth/signup
+//@access Public
 exports.signUp = asyncHandler(async (req, res, next) => {
   const user = await prisma.user.create({
     data: {
@@ -14,22 +16,26 @@ exports.signUp = asyncHandler(async (req, res, next) => {
     },
   });
 
-  const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
-  
+  const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
   delete user.password;
   res.status(201).json({
     status: "success",
     msg: "User created successfully",
     data: user,
-    token
+    token,
   });
 });
 
-
-
+//@desc Login a user
+//@route POST /api/auth/login
+//@access Public
 exports.login = asyncHandler(async (req, res, next) => {
-  
-  const user = await prisma.user.findUnique({ where: { email: req.body.email } });
+  const user = await prisma.user.findUnique({
+    where: { email: req.body.email },
+  });
   let isPasswordMatch = false;
   //find user
   if (user) {
@@ -42,12 +48,14 @@ exports.login = asyncHandler(async (req, res, next) => {
 
   delete user.password;
 
-  const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
+  const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(200).json({
     status: "success",
     msg: "User logged in successfully",
     data: user,
-    token
+    token,
   });
 });

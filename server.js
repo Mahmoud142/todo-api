ea37128c6769cb490b68dc373d431e65e46e95f8
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-09-02 02:18:17 +0300

    improve error message

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use((err, req, res, next) => {
   console.error("Error:", err);
   res.status(500).json({
     status: "error",
+    message: err.message,
     error: "Internal Server Error from error handling middleware",
   });
 });

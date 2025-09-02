5b48a63dd341180e698d0522672bb9c45f56551b
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-09-02 20:20:34 +0300

    Implement Global Route

--- server.js
+++ server.js
@@ -20,11 +20,19 @@ app.use('/api/auth', authRoute);
 app.use('/api/tasks', taskRoute);
 app.use('/api/categories', categoryRoute);
 
+
+
+app.use((req, res, next) => {
+  const error = new Error("This route does not exist");
+  error.statusCode = 404;
+  next(error);
+});
+
 // error handling middleware
 app.use((err, req, res, next) => {
   // console.error("Error:", err);
   // console.log(err);
-  res.status(500).json({
+  res.status(err.statusCode || 500).json({
     status: "error",
     message: err.message || "Internal Server Error",
     statusCode: err.statusCode || 500,

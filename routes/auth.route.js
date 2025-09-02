803a16fa325b46a521bf7b19ebdb99fc7d4f447a
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-09-02 03:08:34 +0300

    validate auth controllers

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -3,8 +3,8 @@ const express = require('express');
 const router = express.Router();
 
 const {signUp,login} = require('../controllers/auth.controller');
-
-router.post('/signup', signUp);
-router.post('/login', login);
+const {signUpValidator,loginValidator} = require('../utils/validators/auth.validator');
+router.post('/signup', signUpValidator, signUp);
+router.post('/login', loginValidator, login);
 
 module.exports = router;
